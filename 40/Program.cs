899b0f521369bbb65019220317856b4cf4d4d426
Aika <akia2710@gmail.com>
2023-06-20 18:47:33 +0300

    HW4

---
+++ 40/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник со сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+
+System.Console.WriteLine("Введите сторону A: ");
+int a  = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите сторону B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите сторону C: ");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a < b + c && b < a + c && c < a + b)
+System.Console.WriteLine("Треугольник существует");
+else
+System.Console.WriteLine("Треугольник не существует");
---
+++ 44/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+System.Console.WriteLine("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int res = 0;
+int num1 = 0; // сосед №1
+int num2 = 0; // сосед №2
+System.Console.Write("0 1");
+for (int i = 2; i < n; i++)
+{
+    res = num1 + num2;
+    System.Console.Write($"{res} ");
+    num1 = num2;
+    num2 = num1;
+}
